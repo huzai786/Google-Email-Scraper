535c2b414ddeac684a218de572cc340274048c25
huzai786 <muhammadhuzaifagamer123@gmail.com>
2022-09-22 10:22:27 -0700

    fix req

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-"""main.py scrapes emails from google search results"""
+"""main.py scrapes emails from Google search results"""
 import sys
 import time
+from datetime import timedelta
 
 try:
-    from fake_useragent import UserAgent
-    from datetime import timedelta
-    from packages.utils import emails_to_file, take_input, get_emails, store_urls
+    from packages.utils import (
+        emails_to_file,
+        take_input,
+        get_emails,
+        store_urls
+    )
 
+    from fake_useragent import UserAgent
 
 except ModuleNotFoundError as e:
     print(e)
@@ -23,16 +28,17 @@ def main():
             time.sleep(delay)
         file_name = store_urls(s, count)
         with open(file_name, "r", encoding="utf-8") as url_file:
-            for i, url in enumerate(url_file):
+            for url_no, url in enumerate(url_file):
                 url = url.strip()
                 if not url.endswith(".pdf"):
-                    time.sleep(req_delay)
+                    if req_delay:
+                        time.sleep(req_delay)
                     emails, msg = get_emails(url.strip(), ua.random)
                     if msg == "break":
                         emails_to_file(s, unique_emails)
                         sys.exit()
 
-                    print(f"url number {i + 1} Status: {msg}")
+                    print(f"url number {url_no + 1} Status: {msg}")
                     if emails:
                         for email in emails:
                             if email not in unique_emails:
@@ -50,11 +56,11 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
+        print('Exiting!')
         sys.exit()
     except RuntimeError:
         sys.exit()
-    except Exception:
-        sys.exit()
+
     end_time = time.perf_counter()
 
     time_to_run = int((end_time - start_time))
--- packages/utils.py
+++ packages/utils.py
@@ -1,21 +1,21 @@
 import os
 import re
 import sys
+
 import requests
 from fake_useragent import UserAgent
 from googlesearch import search
-from typing import Generator
 
 
 def store_urls(search_term: str, total_urls: int) -> str:
-    """Store the urls from google search results"""
+    """Store the urls from Google search results"""
 
-    file_name = re.sub("\s", "_", search_term)
+    file_name = re.sub(r"\s", "_", search_term)
     fpath = os.path.join(os.getcwd(), "files", f"{file_name}_urls.txt")
     print("getting urls....")
 
     try:
-        urls = search(f"{search_term}", num_results=total_urls)
+        urls = set(search(f"{search_term}", num_results=total_urls))
         with open(fpath, "w", encoding="utf-8") as fn:
             for url in urls:
                 fn.write(url)
@@ -35,53 +35,49 @@ def extract_emails(html_text: str) -> list:
     return matches
 
 
-def get_emails(url: str, user_agent: UserAgent) -> list:
+def get_emails(url: str, user_agent: UserAgent) -> tuple[list, str]:
     """Download html content from the url and scrapes and returns emails from it"""
 
     headers = {"User-Agent": f"{user_agent}"}
     emails = []
     try:
-        res = requests.get(url, headers=headers, timeout=5)
+        res = requests.get(url, headers=headers, timeout=8)
         if res.status_code == 200:
             html_text = str(res.text)
             emails = extract_emails(html_text)
             return emails, "Successful"
 
         if res.status_code == 429:  # Too Many Requests
-            return [], "break"
+            return emails, "break"
 
         else:
-            return [], "Unsuccessful"
+            return emails, "Unsuccessful"
 
     except KeyboardInterrupt:
         sys.exit()
 
-    except ConnectionResetError as e:
-        return [], f"Unsuccessful Error: {e}"
-
-    except requests.ReadTimeout as e:
-        return [], f"Unsuccessful Error: {e}"
-
-    except requests.RequestException as e:
-        return [], f"Unsuccessful Error: {e}"
-
     except Exception as e:
-        return [], f"Unsuccessful Error: {e}"
+        return emails, f"Unsuccessful Error: {e}"
 
 
 def take_input():
     """Take input from user"""
 
     print("Enter the search term (comma separated if more than one values): ")
-    search = input("> ")
-    search_list = search.split(",")
+    count = 10
+    search_keyword = input("> ")
+    search_list = search_keyword.split(",")
     search_list = [s.strip() for s in search_list]
     delay = 1
     if len(search_list) > 1:
         print("Delay between each keyword search (seconds): ")
         delay = int(input("> "))
-    print('Delay between each request: ')
-    req_delay = float(input('> '))
+    print('Delay between each request: (leave empty for no delay)')
+    ask_req_delay = input('> ')
+    req_delay = None
+    if ask_req_delay:
+        req_delay = float(ask_req_delay)
+
     while True:
         try:
             count = int(input("Enter the total number of urls to scrape: "))
@@ -95,7 +91,7 @@ def take_input():
 def emails_to_file(search_term: str, emails: list) -> None:
     """Create an absolute file path from the search term and dump emails into it"""
 
-    file_name = re.sub("\s", "_", search_term)
+    file_name = re.sub(r"\s", "_", search_term)
     output_file_path = os.path.join(os.getcwd(), "output", f"{file_name}_emails.txt")
 
     with open(output_file_path, "w", encoding="utf-8") as file_write:
